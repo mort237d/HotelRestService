64bc421f2bc602ffc57f80a04f5b66a38f7df673
Morten <morto1995@hotmail.com>
2019-03-11 10:32:38 +0100

    with connection string

--- HotelRestService/DBUtil/Connection.cs
+++ HotelRestService/DBUtil/Connection.cs
@@ -4,7 +4,7 @@ namespace HotelRestService.DBUtil
 {
     public static class Connection
     {
-        private const string ConnectionString = @"";
+        private const string ConnectionString = @"Data Source=mort237d.database.windows.net;Initial Catalog=MortensDatabase;User ID=KaffeOgKage;Password=Mort237d;Connect Timeout=30;Encrypt=True;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
         public static readonly SqlConnection MyConnection = new SqlConnection(ConnectionString);
     }
 }
